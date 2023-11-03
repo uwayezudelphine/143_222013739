cb9cb030d47265305cf474482476649b55c380a0
uwayezudelphine <149682768+uwayezudelphine@users.noreply.github.com>
2023-11-03 09:55:28 -0700

    Add files via upload

---
+++ operatorexample10.java
@@ -0,0 +1,12 @@
+public class operatorexample10 {
+public static void main(String args[]){  
+    int a=10;  
+    int b=5;  
+    int c=20;  
+    System.out.println(a<b&&a++<c);//false && true = false  
+    System.out.println(a);//10 because second condition is not checked  
+    System.out.println(a<b&a++<c);//false && true = false  
+    System.out.println(a);//11 because second condition is checked  
+    }}   
+    
+
---
+++ operatorexample5.java
@@ -0,0 +1,5 @@
+ class OperatorExample5{  
+public static void main(String args[]){  
+System.out.println(10*10/5+3-1*4/2);  
+}}  
+
