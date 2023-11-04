9290182915dee00d04b79348b4b5684b1cb1b2c5
uwayezudelphine <149682768+uwayezudelphine@users.noreply.github.com>
2023-11-04 11:08:24 -0700

    Update gaga.java

--- gaga.java
+++ gaga.java
@@ -2,7 +2,7 @@ class girl{//create class girl
     public static void main(String args[]){//create main method
         for(int i=0; i<10; i++)//
         {
-        System.out.println("tall");//display tall
+        System.out.println("tall");//display tall in 5 times
 
         }
 
