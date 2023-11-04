526f58ea0aed348b138f5548254fbbe3c6ce624d
uwayezudelphine <149682768+uwayezudelphine@users.noreply.github.com>
2023-11-04 09:47:05 -0700

    Update nestedloop.java

--- nestedloop.java
+++ nestedloop.java
@@ -1,9 +1,9 @@
-class nestedloop{
-    public static void main(String args[]){
-        for(int i=1;i<=5;i++){
+class nestedloop{//create class nestedloop
+    public static void main(String args[]){//create main function
+        for(int i=1;i<=5;i++){ //the firstloop
         
-               for(int j=1;j<=i;j++)   {            
-                   System.out.print("*");
+               for(int j=1;j<=i;j++)   {  //the second loop          
+                   System.out.print("*");//print *
 
                 }
                 System.out.println();
