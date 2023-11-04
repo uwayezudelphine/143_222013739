a347b6c6419e565ba05d4e9cbc4536fdfb9d1e2b
uwayezudelphine <149682768+uwayezudelphine@users.noreply.github.com>
2023-11-04 09:40:54 -0700

    Update nana.java

--- nana.java
+++ nana.java
@@ -1,10 +1,10 @@
-class wl{
-    public static void main(String args[]){
+class wl{// create class wi
+    public static void main(String args[]){//create main method
         int i=0;
-        while(i<10)
+        while(i<10) //while condition
         {
-        System.out.println("beatuful");
-            i++;
+        System.out.println("beatuful");//display beatiful in 7 times
+            i++;//iincrementation
         }
         }
     }
