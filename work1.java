f429efd3bcec8527e9cf93ebdce436a953d7c747
uwayezudelphine <149682768+uwayezudelphine@users.noreply.github.com>
2023-11-04 10:03:04 -0700

    Update work1.java

--- work1.java
+++ work1.java
@@ -1,12 +1,12 @@
-class work{
-    public static void main(String args[]){
-        int a=10;
+class work{//create class 
+    public static void main(String args[]){//create main class
+        int a=10;//initialisation of variable
         int b=15;
-        if( a>b){
-            System.out.println("a is greater");
+        if( a>b){//if condition
+            System.out.println("a is greater");//display a is greater
         }
 else{
-    System.out.println("b is grater");
+    System.out.println("b is grater");//else display b is greater
     }
 }
 }
