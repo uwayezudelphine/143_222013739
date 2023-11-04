dd797055af485551fe787ac7f7d761d6c9d770b6
uwayezudelphine <149682768+uwayezudelphine@users.noreply.github.com>
2023-11-04 11:06:10 -0700

    Update denyse.java

--- denyse.java
+++ denyse.java
@@ -2,7 +2,7 @@ class denyse{//create class denyse
 public static void main(String[]args)//main method
 {
     int i=100; //declation
-       do{ System.out.println(i);//do statement
+       do{ System.out.println(i);//print i in 10 times
         i--;//incrementation i
     }
     while(i>0);// do while condition
