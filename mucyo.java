b084d2a80dd084c48d1a59a7337371616442b504
uwayezudelphine <149682768+uwayezudelphine@users.noreply.github.com>
2023-11-04 09:38:21 -0700

    Update mucyo.java

--- mucyo.java
+++ mucyo.java
@@ -1,9 +1,9 @@
-class boy{
-    public static void main(String args[]){
+class boy{// create class boy
+    public static void main(String args[]){//main method
         int i=2;
 while(i<12){
-    System.out.println("yes");
-    i++;
+    System.out.println("yes");//print yes in 3 times
+    i++;//
       
         } 
     }
