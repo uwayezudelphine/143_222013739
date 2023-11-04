ea64767478376b7b574f72ae8363186c4dedede9
uwayezudelphine <149682768+uwayezudelphine@users.noreply.github.com>
2023-11-04 11:07:17 -0700

    Update dowhile.java

--- dowhile.java
+++ dowhile.java
@@ -1,7 +1,7 @@
  class dw{// create class dw
     public static void main(String args[]){//main method
         int i=1;
-        do{Sytem.out.println(i);// do while statement
+        do{Sytem.out.println(i);// print i in 5 times
         i++;
     }
     while(i<10);//do while condition
