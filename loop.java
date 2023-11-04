c0a63b9ac3dc9278fb8299468e4e859f7c0a70e9
uwayezudelphine <149682768+uwayezudelphine@users.noreply.github.com>
2023-11-04 09:35:40 -0700

    Update loop.java

--- loop.java
+++ loop.java
@@ -1,10 +1,11 @@
-class delphine{
-    public static void main(String args[]){
+class delphine{//create class delphine
+    public static void main(String args[]){//main method
+
         int i=0;
-        do {System.out.println("your are girl");
+        do {System.out.println("your are girl");//print girl in 5 times
         i++;
         }
-        while(i<5);
+        while(i<5); //do while condition
     }
 }
 
