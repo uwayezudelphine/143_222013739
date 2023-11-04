cb472f775c277c1f91f94fc06417097d1fff1765
uwayezudelphine <149682768+uwayezudelphine@users.noreply.github.com>
2023-11-04 10:12:56 -0700

    Update work2.java

--- work2.java
+++ work2.java
@@ -1,20 +1,20 @@
- class work2 {
-public static void main(String args[]){
-    int salary = 3000;
-    if(salary>0 && salary<1000){
-        System.out.println("your are very poor");
+ class work2 {//create class work2
+public static void main(String args[]){// create main method
+    int salary = 3000; //declaration of variable
+    if(salary>0 && salary<1000){//if salary greater than0 &lessthan1000
+        System.out.println("your are very poor"); //print your are very poor
     
 }
-else if(salary>1000 &&salary<20000){
-    System.out.println("your are poor");
+else if(salary>1000 &&salary<20000){//if else salary greater1000&less20000
+    System.out.println("your are poor");//print your poor
 }
-else if(salary>20000 && salary<50000){
-System.out.println("your are middle");
+else if(salary>20000 && salary<50000){//if salary greater than20000&less than50000
+System.out.println("your are middle");//print your are middle
 }
 
-else if(salary>50000 &&salary<=700000){
-    System.out.println("your are rich");
+else if(salary>50000 &&salary<=700000){ //if salary greaterthan50000&lessthan70000
+    System.out.println("your are rich");//print your are rich
 }
     else{
-      System.out.println("your very rich");
+      System.out.println("your very rich");//else your are very rich
     }}}
