dcdc2c91a13048990996a951f73a5dcc1f2183c4
uwayezudelphine <149682768+uwayezudelphine@users.noreply.github.com>
2023-11-04 10:41:07 -0700

    Update delphine.java

--- delphine.java
+++ delphine.java
@@ -1,9 +1,9 @@
-class dw{
-    public static void main(String args[]){
-        int i=1;
-        do{System.out.println(i);
-    i++;
+class dw{//create class dw
+    public static void main(String args[]){//main method
+        int i=1;//declation variable i 
+        do{System.out.println(i);// dowhile statement
+    i++;//incrementation i
     }
-while(i<10);
+while(i<10);// while condition
     }
 }
