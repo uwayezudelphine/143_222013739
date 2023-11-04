0389345d4056f72a116664278faae4ceae3044d8
uwayezudelphine <149682768+uwayezudelphine@users.noreply.github.com>
2023-11-04 09:53:08 -0700

    Update simpleInterest.java

--- simpleInterest.java
+++ simpleInterest.java
@@ -1,17 +1,17 @@
-public class simpleInterest {
+public class simpleInterest {//create class compoundInterest
 
-    public static void main(String[] args) {
-      interest interest1= new interest();
-       interest1.calculate(); 
+    public static void main(String[] args) {//main method
+      interest interest1= new interest();// create object interest1
+       interest1.calculate(); //coll method calculate
     }
 }
 
 //class to calculate simple interest
 
-class interest {
-  public  void calculate(){
-    float principle=100000,rate=5,time=5,simpleInterest;
-    simpleInterest=(principle*rate*time)/100;
-    System.out.println("simple interest for Bob After 5 years is ="+simpleInterest);
+class interest {//class interest
+  public  void calculate(){//create function
+    float principle=100000,rate=5,time=5,simpleInterest;//initiatilisation of variable
+    simpleInterest=(principle*rate*time)/100;//calculate simpleinterest
+    System.out.println("simple interest for Bob After 5 years is ="+simpleInterest);//display simple interest after5 years
   }
 }
