99ee260461906078d31cb2e21c716e17b3a892ef
uwayezudelphine <149682768+uwayezudelphine@users.noreply.github.com>
2023-11-04 23:47:40 -0700

    Update break.java

--- break.java
+++ break.java
@@ -1,10 +1,10 @@
-class breek{
-    public static void main( String args[]){
+class breek{//class breek
+    public static void main( String args[]){//main method
         for( int i=1;i<10;i++){
             if(i==5){
                 break;
             }
-            System. out.println(i);
+            System. out.println(i);//print i in 7 times
         }
     }
 }
