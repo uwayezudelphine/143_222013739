47f48db2d83f54d197f74e500eb4eb56d14ad703
uwayezudelphine <149682768+uwayezudelphine@users.noreply.github.com>
2024-02-20 13:38:58 +0200

    Delete tourism_management_system directory

--- tourism_management_system/dist/javadoc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"tourism_management_system"}];updateSearchResults();
--- tourism_management_system/dist/javadoc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"tourism_management_system","l":"Home"},{"p":"tourism_management_system","l":"login"},{"p":"tourism_management_system","l":"sinup"},{"p":"tourism_management_system","l":"Tourism_management_system"}];updateSearchResults();
